fe1a6f4963d2d7219107450c589d9e74860a421d
Nick Cruz <nickcruz920@gmail.com>
2023-03-03 08:44:04 -0500

    Created generateSQL and resetSQL function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function generateSQL() {
+  let output = document.getElementById("sqlOutput");
+  
+  output.innerHTML = `<h2>SELECT * FROM [table name]`;
+}
 
+function resetSQL() {
+  let output = document.getElementById("sqlOutput");
+  
+  output.innerHTML = "SQL Code Goes Here...";
+}
