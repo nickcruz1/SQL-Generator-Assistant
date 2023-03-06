e64931fbd3d8732d575c1a229ef20a5c5fdfe43d
Nick Cruz <nickcruz920@gmail.com>
2023-03-06 14:53:50 -0500

    Select statement inputs (an array)

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // CREATE MULTIPLE FUNCTIONS FOR DIFFERENT SQL STATEMENTS (PROBABLY WILL USE SWITCH CASES) AND THEN PUT ALL FUNCTIONS INTO ONE "MASTER FUNCTION" AND ASSIGN IT TO THE GENERATE SQL BUTTON...
 
+<!-- Select Statement Inputs -->
+let selectInputs = [
+  "How Do You Select Everything from Database", "How To Select Everything from a Database", "How To Select All", "How To Select a column from a database", "How To Select A Column from Database", "How to Select many columns from a database", 
+]
+
 function generateSQL() {
   let output = document.getElementById("sqlOutput");
   
